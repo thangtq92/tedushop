d045368995809a41dbf0d8c3498ac3ac0ba69ef7
Trần Quyết Thắng <thangtq92@gmail.com>
2017-02-21 14:47:32 +0700

    How to use web api

---
+++ TeduShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+
+namespace TeduShop.UnitTest.ServiceTest
+{
+    [TestClass]
+   public  class PostCategoryServiceTest
+    {
+        private Mock<IPostCategoryRepository> _mockRepository;
+        private Mock<IUnitOfWork> _mockUnitOfWork;
+        private IPostCategoryService _categoryService;
+        private List<PostCategory> _listCategory;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<IPostCategoryRepository>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _categoryService = new PostCategoryService(_mockRepository.Object,_mockUnitOfWork.Object);
+            _listCategory = new List<PostCategory>()
+            {
+                new PostCategory() {ID = 1, Name = "Danh mục 1", Status = true},
+                new PostCategory() {ID = 2, Name = "Danh mục 2", Status = true},
+                new PostCategory() {ID = 3, Name = "Danh mục 3", Status = true},
+                new PostCategory() {ID = 4, Name = "Danh mục 4", Status = true},
+                new PostCategory() {ID = 5, Name = "Danh mục 5", Status = true}
+            };
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_GetAll()
+        {
+            //setup method
+            _mockRepository.Setup(m => m.GetAll(null)).Returns(_listCategory);
+
+            //call action
+           var result =  _categoryService.GetAll() as List<PostCategory>;
+            //compare
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test";
+            category.Alias = "Test";
+            category.Status = true;
+            _mockRepository.Setup(m => m.Add(category)).Returns((PostCategory p) =>
+            {
+                p.ID = 1;
+                return p;
+            });
+            var result = _categoryService.Add(category);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1,result.ID);
+        }
+    }
+}
